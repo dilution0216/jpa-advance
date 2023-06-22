f581511d6fc62c6ccec2c9291b847e0a3cc782e0
dilution0216 <hglad0216@naver.com>
2023-06-23 01:53:41 +0900

    OnetoOne 단방향 테스트

---
+++ src/main/java/com/sparta/jpaadvance/repository/FoodRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.jpaadvance.repository;
+
+import com.sparta.jpaadvance.entity.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
