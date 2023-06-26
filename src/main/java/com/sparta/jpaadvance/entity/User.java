6fcb8dc06ccbff7997e998a05b40b67becc9ee47
dilution0216 <hglad0216@naver.com>
2023-06-26 19:45:52 +0900

    영속성 전이 (저장,삭제)

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,6 +17,13 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user")
-    private List<Food> foodList = new ArrayList<>(); // 실제로 이렇게는 불가, 관계를 표현하는방법. 데이터베이스에 실제로 이렇게 저장되진 않는다.
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Food> foodList = new ArrayList<>();
+    // 실제로 이렇게는 불가, 관계를 표현하는방법. 데이터베이스에 실제로 이렇게 저장되진 않는다.
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this);
+        // 외래 키 설정
+    }
 }
